b5c4d96317264ea09ff39fa433a39d24c95a2c72
ahmed <ahmed.ali@diracsystems.com>
2025-05-25 16:17:39 +0300

    solve issue in production not see cookies

--- src/app/[locale]/user/[id]/metadata.tsx
+++ src/app/[locale]/user/[id]/metadata.tsx
@@ -1,14 +1,20 @@
 import { constructMetadata } from '@/lib/metadata';
 import { cookies } from 'next/headers';
+import { headers } from 'next/headers';
 
 export async function getUserMetadata(locale: string, id: string) {
   const messages = (await import(`../../../../messages/${locale}.json`)).default;
   const cookieStore = await cookies();
-
+  const headerList = await headers();
+  const cookieHeader = headerList.get('cookie') || '';
+  const token = cookieHeader
+    .split('; ')
+    .find((row) => row.startsWith('token='))
+    ?.split('=')[1] || '';
   const authToken = cookieStore.get('token')?.value || '';
   const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}/api/users/${id}`, {
     headers: {
-      Authorization: `Bearer ${authToken}`,
+      Authorization: `Bearer ${authToken ? authToken : token}`,
     },
   }); 
   const user = await response.json();
